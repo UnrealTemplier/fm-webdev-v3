362be79d0b535afbdd88e8073e451655e3c42837
Sergey Evseev <jaimore.evseev@gmail.com>
2024-08-03 03:57:35 +0400

    JS/ Numbers, booleans and strings.

--- 3. JavaScript/experiments.js
+++ 3. JavaScript/experiments.js
@@ -1,6 +1,29 @@
-console.log("Luke, I am your father!");
-console.log("Hello, world!");
+const intNumber = 25;
+const floatNumber = 12.5;
+const sum = intNumber + floatNumber;
 
-const monthlyRent = 500;
-let yearlyRent = monthlyRent * 12;
-console.log(yearlyRent);
+console.log("intNumber = " + intNumber);
+console.log('floatNumber = ' + floatNumber);
+console.log(`sum = ${sum}`);
+console.log("");
+
+let lightIsOn = true;
+let skyIsRed = false;
+console.log(`lightIsOn = ${lightIsOn}`);
+console.log(`skyIsRed = ${skyIsRed}`);
+console.log("");
+
+const firstName = "Unreal";
+let lastName = 'Templier';
+let greeting = "Hello " + firstName + ' ' + lastName + "! How are you!?";
+const templaterGreeting = `Hello ${firstName} ${lastName}! How are you!?`;
+console.log('greeting: ' + greeting);
+console.log("templateGreeting: " + templaterGreeting);
+console.log("");
+
+console.log("Single 'quotes' inside double quotes.");
+console.log('Double "quotes" inside single quotes');
+console.log(`'Single' and "Double" quotes inside backticks`);
+console.log("Escape characters inside double quotes: \" ' \\ ");
+console.log('Escape characters inside single quotes: " \' \\ ');
+console.log(`Escape characters inside backticks: " ' \\ `);
