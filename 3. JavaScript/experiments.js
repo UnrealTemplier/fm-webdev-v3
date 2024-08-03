c1dce1149c71bddd2666cd7f496c05aa7bba068c
Sergey Evseev <jaimore.evseev@gmail.com>
2024-08-03 04:20:49 +0400

    JS/ Control flow.

--- 3. JavaScript/experiments.js
+++ 3. JavaScript/experiments.js
@@ -1,29 +1,22 @@
-const intNumber = 25;
-const floatNumber = 12.5;
-const sum = intNumber + floatNumber;
+const skyIsBlue = true;
+if (skyIsBlue) {
+    console.log("Sky is blue!");
+} else {
+    console.log("Sky is not blue?");
+}
 
-console.log("intNumber = " + intNumber);
-console.log('floatNumber = ' + floatNumber);
-console.log(`sum = ${sum}`);
-console.log("");
+let doubleEqual = (2 + 2) == "4";
+let tripleEqual = (2 + 2) === "4";
+console.log(`doubleEqual = ${doubleEqual}`);
+console.log(`tripleEqual = ${tripleEqual}`);
 
-let lightIsOn = true;
-let skyIsRed = false;
-console.log(`lightIsOn = ${lightIsOn}`);
-console.log(`skyIsRed = ${skyIsRed}`);
-console.log("");
-
-const firstName = "Unreal";
-let lastName = 'Templier';
-let greeting = "Hello " + firstName + ' ' + lastName + "! How are you!?";
-const templaterGreeting = `Hello ${firstName} ${lastName}! How are you!?`;
-console.log('greeting: ' + greeting);
-console.log("templateGreeting: " + templaterGreeting);
-console.log("");
-
-console.log("Single 'quotes' inside double quotes.");
-console.log('Double "quotes" inside single quotes');
-console.log(`'Single' and "Double" quotes inside backticks`);
-console.log("Escape characters inside double quotes: \" ' \\ ");
-console.log('Escape characters inside single quotes: " \' \\ ');
-console.log(`Escape characters inside backticks: " ' \\ `);
+const friendsAtParty = 10;
+if (friendsAtParty < 0) {
+    console.log(`Wrong friends number!`);
+} else if (friendsAtParty === 0) {
+    console.log(`Nobody at the party.`);
+} else if (friendsAtParty <= 4) {
+    console.log(`We can play Mario Kart!`);
+} else {
+    console.log(`Let's dance!`);
+}
