22a781311232e02173659ae0af5d461a376b9c42
Sergey Evseev <jaimore.evseev@gmail.com>
2024-08-13 23:58:32 +0400

    Putting It All Together/ Events and Listeners.

--- 3. JavaScript/experiments.js
+++ 3. JavaScript/experiments.js
@@ -1,16 +1,25 @@
-const body = document.querySelector("body");
-body.style.backgroundColor = "black";
-body.style.color = "white";
-
-const square = document.querySelector(".red-square");
-square.style.backgroundColor = "blue";
-square.style.width = "200px";
-square.style.border = "1px solid white";
-square.style.borderRadius = "5px";
-square.innerHTML = "Changed by JS!";
-square.style.color = "white";
-
-const listItems = document.querySelectorAll(".js-target");
-listItems.forEach(function(item) {
-    item.innerHTML = "<em>Changed by JS!</em>";
+const button = document.querySelector(".event-button");
+button.addEventListener("click", function() {
+    alert("Button clicked!");
 });
+
+
+const input = document.querySelector(".event-input");
+const paragraph = document.querySelector(".event-paragraph");
+input.addEventListener("keyup", function() {
+    paragraph.innerText = input.value;
+});
+
+
+const colorBox = document.querySelector(".color-box");
+const colorInput = document.querySelector(".color-input");
+
+colorInput.addEventListener("change", function () {
+    colorBox.style.backgroundColor = colorInput.value;
+});
+
+
+const buttonContainer = document.querySelector(".button-container")
+                                .addEventListener("click", function(event) {
+                                    alert(`You cliked on button ${event.target.innerText}`);
+                                });
